add6cef7d6831a2c79f48dc2a3c32e686d327c68
Chris Engelsma <chris.engelsma@gmail.com>
2020-12-16 14:50:39 -0600

    added gulp stuff

---
+++ Gulpfile.js
@@ -0,0 +1,100 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const useref = require('gulp-useref');
+const uglify = require('gulp-uglify');
+const gulpIf = require('gulp-if');
+const cssnano = require('gulp-cssnano');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const deploy = require('gulp-gh-pages');
+const del = require('del');
+const runSequence = require('run-sequence');
+const flatten = require('gulp-flatten');
+
+// Development Tasks
+// -----------------
+
+// Start browserSync server
+gulp.task('browserSync', function () {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+});
+
+gulp.task('sass', function () {
+  return gulp.src('app/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
+    .pipe(flatten())
+    .pipe(gulp.dest('app/css')) // Outputs it in the css folder
+    .pipe(browserSync.reload({ // Reloading with Browser Sync
+      stream: true
+    }));
+});
+
+// Watchers
+gulp.task('watch', function () {
+  gulp.watch('app/**/*.scss', [ 'sass' ]);
+  gulp.watch('app/**/*.html', browserSync.reload);
+  gulp.watch('app/**/*.js', browserSync.reload);
+});
+
+// Optimization Tasks 
+// ------------------
+
+// Optimizing CSS and JavaScript 
+gulp.task('useref', function () {
+
+  return gulp.src(['app/**/*.html', '!app/lib/**/*'])
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+// Optimizing Images 
+gulp.task('images', function () {
+  return gulp.src('app/assets/**/*.+(png|jpg|jpeg|gif)')
+    // Caching images that ran through imagemin
+    .pipe(cache(imagemin({ interlaced: true })))
+    .pipe(gulp.dest('dist/assets'));
+});
+
+// Copying fonts
+// gulp.task('fonts', function () {
+//   return gulp.src('app/lib/font-awesome/fonts/*.+(otf|eot|svg|ttf|woff|woff2)')
+//     .pipe(gulp.dest('dist/fonts'));
+// });
+
+// Cleaning
+gulp.task('clean', function () {
+  return del.sync('dist').then(function (cb) {
+    return cache.clearAll(cb);
+  });
+});
+
+gulp.task('clean:dist', function () {
+  return del.sync([ 'dist/**/*', '!dist/assets' ]);
+});
+
+// Deploy
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(deploy());
+});
+
+// Build Sequences
+// ---------------
+
+gulp.task('default', function (callback) {
+  runSequence([ 'sass', 'browserSync' ], 'watch', callback);
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', 'sass', [ 'useref', 'images' ], callback);
+});
--- app/index.route.js
+++ app/index.route.js
@@ -5,14 +5,13 @@
     .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
       $stateProvider
         .state('main', {
-          url: '/main',
+          url: '/',
           controller: 'View1Controller',
           controllerAs: 'vm',
           templateUrl: 'view1/view1.html'
         });
 
-      $urlRouterProvider.when('/', '/main');
-      $urlRouterProvider.otherwise('/main');
+      $urlRouterProvider.otherwise('/');
 
       $locationProvider.html5Mode(true);
     });
